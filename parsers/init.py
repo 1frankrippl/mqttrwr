a8e50ef9fb30cb23d3c9ad14e051a6e156ee0f35
bejaehringerhof <107788434+1frankrippl@users.noreply.github.com>
2025-11-01 21:07:46 +0100

    Update init.py

--- parsers/init.py
+++ parsers/init.py
@@ -1,10 +1,14 @@
 from .wh_parser import parse_wh
 from .battery_parser import parse_battery
+from .temperature_parser import parse_temperature
+from .humidity_parser import parse_humidity
 
 def get_parser(name):
     parsers = {
         "wh_parser": parse_wh,
         "battery_parser": parse_battery,
+        "temperature_parser": parse_temperature,
+        "humidity_parser": parse_humidity,
         # weitere Parser hier registrieren
     }
     return parsers.get(name, lambda x: x)  # Fallback: Original zurückgeben
