a10dd7816787ee00bdb1a3a082a9aaa83f94db95
bejaehringerhof <107788434+1frankrippl@users.noreply.github.com>
2025-11-01 21:30:15 +0100

    Update init.py

--- parsers/init.py
+++ parsers/init.py
@@ -2,6 +2,8 @@ from .wh_parser import parse_wh
 from .battery_parser import parse_battery
 from .temperature_parser import parse_temperature
 from .humidity_parser import parse_humidity
+from .exti_parser import parse_exti
+from .timestamp_parser import parse_timestamp
 
 def get_parser(name):
     parsers = {
@@ -9,6 +11,8 @@ def get_parser(name):
         "battery_parser": parse_battery,
         "temperature_parser": parse_temperature,
         "humidity_parser": parse_humidity,
+        "exti_parser": parse_exti,
+        "timestamp_parser": parse_timestamp,
         # weitere Parser hier registrieren
     }
     return parsers.get(name, lambda x: x)  # Fallback: Original zurückgeben
